c435f92d63fa13a57b85862fbea733f31831b86e
HOYEONCHOI <chy1995@ajou.ac.kr>
2021-12-20 23:26:21 +0900

    섹션6. 주문 도메인 개발

--- main/java/jpabook/jpashop/domain/OrderItem.java
+++ main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Fetch;
 
@@ -9,6 +11,7 @@ import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -26,4 +29,40 @@ public class OrderItem {
     private int orderPrice; // 주문 당시 가격 (가격은 변동 될 수 있으므로 필요)
 
     private int count;  // 주문 당시 수량
+
+    // JPA는 protected 까지 기본생성자를 만들도록 허용
+    /*protected OrderItem(){
+
+    }*/
+    /**
+     * 이렇게 했을 때 장점은, OrderService.java에서
+     * OrderItem orderItem = new OrderItem() 같은 코드를 쓰지 말라는 것을 인지시킬 수 있다.
+     * 여러 방식으로 코드를 만들도록 분산시키지 않고, createOrderItem 만을 사용하도록 제약시키는 것이 유지보수에 좋다.
+     */
+    // 위 코드는 @NoArgsConstructor(access = AccessLevel.PROTECTED)로 줄일 수 있다.
+
+    // 생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
+    // 비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    // 조회 로직
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
---
+++ main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,69 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count){
+
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장 (CascadeType.ALL 옵션을 주어서 delivery, orderItem 이 자동으로 persist 된다.)
+        // CascadeType.ALL의 경우 private Owner인 경우만 사용한다.
+        // ex. delivery, orderItem 은 order 엔티티 에서만 참조하고 사용한다.
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId){
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문 취소
+        order.cancel();
+    }
+
+    /**
+     * 검색
+     */
+    /*public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAll(orderSearch);
+    }*/
+}
